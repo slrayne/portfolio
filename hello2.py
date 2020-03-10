dfa783c5c5635f607aa6df2403da8ff5552bcee1
slrayne <samuelrayne96@gmail.com>
2020-03-10 03:23:50 -0400

    hello + module calling script written in python

---
+++ hello2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#Author: Samuel Rayne
+#version: 1.0
+'''
+this is a text comment
+'''
+
+username=input ("What is your name?")
+#respond in the shell
+
+if username == " Flatiron":
+    print("hello future security ninja")
+else:
+    print("Hello " + username)
